23adcafb793b85ebeb9404ed4a2fdff36d510286
WebMats <lorlando127@gmail.com>
2019-06-14 13:45:42 -0400

    finished MVP and stretch

--- Data_Structures_Answers.md
+++ Data_Structures_Answers.md
@@ -13,9 +13,15 @@ Add your answers to the questions below.
     Ans: O(n)
 
 5. What is the runtime complexity of the provided code in `names.py`?
+    Ans: O(n^2)
 
 6. What is the space complexity of the provided code in `names.py`?
+    Ans: O(n)
+     Worst case, if name sets are exact duplicates, then 'duplicates' array will have to hold 'n' names
 
 7. What is the runtime complexity of your optimized code in `names.py`?
+    Ans: O(n)
 
 8. What is the space complexity of your optimized code in `names.py`?
+    Ans: O(2n)
+    Worst case, Binary Search Tree holds n values, and if name sets are exact duplicates, then 'duplicates' array will have to hold 'n' names.
--- names/names.py
+++ names/names.py
@@ -2,19 +2,82 @@ import time
 
 start_time = time.time()
 
-f = open('names_1.txt', 'r')
+f = open('./names/names_1.txt', 'r')
 names_1 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
-f = open('names_2.txt', 'r')
+f = open('./names/names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
 duplicates = []
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+
+class BinarySearchTree:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+        
+    def insert(self, value):
+        if value < self.value:
+          if not self.left:
+            self.left = BinarySearchTree(value)
+          else:
+            self.left.insert(value)
+        elif value >= self.value:
+          if not self.right:
+            self.right = BinarySearchTree(value)
+          else:
+            self.right.insert(value)
+        pass
+
+    def contains(self, target):
+        if self.value == target:
+          return True
+        elif target < self.value and not self.left == None:
+          return self.left.contains(target)
+        elif target >= self.value and not self.right == None:
+          return self.right.contains(target)
+        else:
+          return False
+        pass
+
+name1_bst = BinarySearchTree(names_1[0])
+
+for i in range(1, len(names_1)):
+    name1_bst.insert(names_1[i])
+
+for j in range(len(names_2)):
+    if name1_bst.contains(names_2[j]):
+        duplicates.append(names_2[j])
+
+
+end_time = time.time()
+print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
+print (f"runtime: {end_time - start_time} seconds")
+
+
+
+
+# Stretch Solution
+print('\n STRETCH OUTPUT')
+names_1.sort()
+duplicates = []
+
+for name_2 in names_2:
+    names_1_sorted = [*names_1]
+    half = len(names_1_sorted) // 2
+    found = False
+    while found != True and half >= 1:
+        if names_1_sorted[half] == name_2:
+            found = True
+            duplicates.append(name_2)
+        elif names_1_sorted[half] < name_2:
+            names_1_sorted = names_1_sorted[half:]
+            half = len(names_1_sorted) // 2
+        elif names_1_sorted[half] > name_2:
+            names_1_sorted = names_1_sorted[:half]
+            half = len(names_1_sorted) // 2
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
